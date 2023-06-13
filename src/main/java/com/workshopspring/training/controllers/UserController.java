8eb23bb8f1de64a7a2f42fa9acba08bd7b34d9a3
Monteir0x <msmonteiro@id.uff.br>
2023-06-13 17:55:03 -0300

    Service layer, component registration

--- src/main/java/com/workshopspring/training/controllers/UserController.java
+++ src/main/java/com/workshopspring/training/controllers/UserController.java
@@ -1,17 +1,33 @@
 package com.workshopspring.training.controllers;
 
 import com.workshopspring.training.entities.User;
+import com.workshopspring.training.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria","maria@gmail.com", "123456789", "123456789");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
