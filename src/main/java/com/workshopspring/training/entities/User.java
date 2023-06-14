343edc77d246b7b806af697715fd691139cfe974
Monteir0x <msmonteiro@id.uff.br>
2023-06-14 12:25:58 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/workshopspring/training/entities/User.java
+++ src/main/java/com/workshopspring/training/entities/User.java
@@ -1,9 +1,12 @@
 package com.workshopspring.training.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 @Entity
 @Table(name = "tb_user")
@@ -17,6 +20,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    @OneToMany(mappedBy = "client")
+    @JsonIgnore
+    private List<Order> orders = new ArrayList<>();
 
     public User(){}
 
@@ -66,6 +72,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
---
+++ src/main/java/com/workshopspring/training/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.workshopspring.training.services;
+
+import com.workshopspring.training.entities.Order;
+import com.workshopspring.training.repositories.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public Order findById(Long id) {
+        Optional<Order> obj = orderRepository.findById(id);
+        return obj.get();
+    }
+}
