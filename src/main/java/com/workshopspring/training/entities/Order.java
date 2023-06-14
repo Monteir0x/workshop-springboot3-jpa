a0c3a37dedaca0c6ef63968f61ef34fe3debbd46
Monteir0x <msmonteiro@id.uff.br>
2023-06-14 13:51:01 -0300

    OrderStatus enum

--- src/main/java/com/workshopspring/training/entities/Order.java
+++ src/main/java/com/workshopspring/training/entities/Order.java
@@ -1,6 +1,7 @@
 package com.workshopspring.training.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.workshopspring.training.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -15,6 +16,8 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-MM-dd'T'HH:mm:ss.ss'Z'", timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -22,10 +25,21 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public Long getId() {
