d08a8955aa5f968e5f37f47151ffbc18e0eef478
Monteir0x <msmonteiro@id.uff.br>
2023-06-15 16:38:39 -0300

    User insert

--- src/main/java/com/workshopspring/training/controllers/UserController.java
+++ src/main/java/com/workshopspring/training/controllers/UserController.java
@@ -2,12 +2,12 @@ package com.workshopspring.training.controllers;
 
 import com.workshopspring.training.entities.User;
 import com.workshopspring.training.services.UserService;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,5 +29,16 @@ public class UserController {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = userService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        user=userService.insert(user);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
